a73e99a85cef98b3356e065597b3e87f96842e87
Douglas Martins <martins.douglas.souza@gmail.com>
2018-10-10 20:17:18 -0300

    Algoritmo K-Guloso para escolha do primeiro elemento otimizado

--- Projeto/MaximumDiversityProblem/src/Estrategias/KGuloso.java
+++ Projeto/MaximumDiversityProblem/src/Estrategias/KGuloso.java
@@ -3,7 +3,9 @@ package Estrategias;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class KGuloso implements EstrategiaSelecao {
 
@@ -46,8 +48,8 @@ public class KGuloso implements EstrategiaSelecao {
             - Escolher aleatoriamente um dos índices e colocar no vetor 'solucao' o valor 'true' na posição correspondente ao índice escolhido
          */
         int soma;
-        HashMap<Integer, Integer> somatorioLinhasComIndices = new HashMap<>();
-        HashMap<Integer, Integer> kElementosEscolhidos = new HashMap<>();
+        Map<Integer, Integer> somatorioLinhasComIndices = new HashMap<>();
+        List<Integer> kElementosEscolhidos = new ArrayList<>();        
 
         // Obtendo soma de cada linha
         for (int i = 0; i < matriz.length; i++) {
@@ -59,35 +61,37 @@ public class KGuloso implements EstrategiaSelecao {
                 //Adicionando em um Hashmap a soma obtida com o índice da linha
                 somatorioLinhasComIndices.put(i, soma);
             }
-        }                
+        }
 
-        // Selecionandos os índices de acordo com 'k'
-        for (int i = 0; i < this.k; i++) {
+        // Criando uma lista com os índices organizados pelo seu respectivo valor de forma crescente
+        List<Integer> indicesOrganizados = new ArrayList<>();
+        organizarMapPorValor(somatorioLinhasComIndices).forEach((entry) -> {
+            indicesOrganizados.add(entry.getKey());
+        });
 
-            int valorMaior = 0;
-            int indiceValorMaior = -1;
-            for (int j = 0; j < somatorioLinhasComIndices.size(); j++) {
-
-                //Verifico se, de acordo com o índice 'J', já existe este indice cadastrado
-                //No Hashmap de kElementosEscolhidos
-                if (!kElementosEscolhidos.containsKey(j) && somatorioLinhasComIndices.get(j) > valorMaior) {
-                    valorMaior = somatorioLinhasComIndices.get(j);
-                    indiceValorMaior = j;
-                }
-            }
-
-            // removendo a linha cujo a soma já foi calculada            
-            kElementosEscolhidos.put(indiceValorMaior, valorMaior);
-
+        // Escolhendo os 'k' elementos mais dispersos (pegando os 'k' elementos do final da lista)
+        for(int i = 1; i <= this.k; i++){
+            kElementosEscolhidos.add(indicesOrganizados.get(indicesOrganizados.size() - i));
         }
 
         // Escolher aleatoriamente um dos índices...        
-        int posicaoAleatoria = new Random().nextInt(kElementosEscolhidos.size() - 1) * 10;
-        System.out.println("Posição aleatória: " + posicaoAleatoria);
+        int posicaoAleatoria = (int) ((Math.random() * kElementosEscolhidos.size()));        
 
         // Gravar no vetor de 'solucao' o valor 'true' ...
         solucao[posicaoAleatoria] = true;
+        indiceSelecionados.add(posicaoAleatoria);
+    }
 
+    /*
+    * Método que orgniza o Map em ordem crescente
+     */
+    private <K, V extends Comparable<? super V>> SortedSet<Map.Entry<K, V>> organizarMapPorValor(Map<K, V> map) {
+        SortedSet<Map.Entry<K, V>> sortedEntries = new TreeSet<>((Map.Entry<K, V> e1, Map.Entry<K, V> e2) -> {
+            int res = e1.getValue().compareTo(e2.getValue());
+            return res != 0 ? res : 1;
+        });
+        sortedEntries.addAll(map.entrySet());
+        return sortedEntries;
     }
 
 }
