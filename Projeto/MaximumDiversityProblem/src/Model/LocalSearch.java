83dfff04d710053df470f435b63f743ac7281bdf
Tiago Funk <tiagoff.tf@gmail.com>
2018-10-25 11:40:11 -0300

    Apaguei um while true que impedia do programa avançar

--- Projeto/MaximumDiversityProblem/src/Model/LocalSearch.java
+++ Projeto/MaximumDiversityProblem/src/Model/LocalSearch.java
@@ -15,22 +15,19 @@ public class LocalSearch {
     }
     
     public void localSearch(Solution s){
-        while (true){
-            try{
-            Solution newSol = (Solution) s.clone();            
+        try{
+            Solution newSol = (Solution) s.clone();
             newSol.set = s.set;
             strategy.localSearch(newSol);
             newSol.evaluate();
-            
-            if (newSol.value > s.value){
+
+            if (newSol.value > s.value) {
                 s = newSol;
-            }else break;            
-            
-            }catch(CloneNotSupportedException e){
-                e.printStackTrace();
-                break;
-            }            
-        }
+            } 
+
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }    
     }
     
 }
