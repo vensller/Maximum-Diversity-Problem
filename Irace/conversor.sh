c75d174944d1bb2ebb841d4c65d2ca4b69f20b14
Tiago Funk <tiagoff.tf@gmail.com>
2018-10-31 02:45:43 -0300

    Correção de bug no alfaguloso

--- Irace/conversor.sh
+++ Irace/conversor.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+cp "/home/tiago/Repositorios/Github/Maximum-Diversity-Problem/Projeto/MaximumDiversityProblem/dist/MaximumDiversityProblem.jar" "/home/tiago/Repositorios/Github/Maximum-Diversity-Problem/Irace"
+
 cat stub.sh MaximumDiversityProblem.jar > target-runner && chmod +x target-runner
