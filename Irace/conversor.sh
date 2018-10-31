676bb1e5c4893590efdf4728f68886cc86777b3c
Tiago Funk <tiagoff.tf@gmail.com>
2018-10-31 18:21:07 -0300

    Mudanças nos scripts

--- Irace/conversor.sh
+++ Irace/conversor.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cp "/home/tiago/Repositorios/Github/Maximum-Diversity-Problem/Projeto/MaximumDiversityProblem/dist/MaximumDiversityProblem.jar" "/home/tiago/Repositorios/Github/Maximum-Diversity-Problem/Irace"
-
 cat stub.sh MaximumDiversityProblem.jar > target-runner && chmod +x target-runner
---
+++ Irace/copiar.sh
@@ -0,0 +1,1 @@
+cp "/home/usuario/Repositorios/Github/Maximum-Diversity-Problem/Projeto/MaximumDiversityProblem/dist/MaximumDiversityProblem.jar" "/home/usuario/Repositorios/Github/Maximum-Diversity-Problem/Irace"
