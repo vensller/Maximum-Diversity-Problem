2ada5ee367fdd3076540ea8099353be1f4a5edf6
Tiago Funk <tiagoff.tf@gmail.com>
2018-11-19 17:46:28 -0200

    Arrumando os scripts

--- Scripts/experimento_mdg_b.sh
+++ Scripts/experimento_mdg_b.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+cd /home/usuario/Downloads/mdplib_2010/MDG_b/
+
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-	echo "\n\n\n\n\nInteração $i"
+	echo "Interação $i:"
 	for arquivo in /home/usuario/Downloads/mdplib_2010/MDG_b/*
 		do
 			if [ -f "$arquivo" ]
 			then
-				echo $arquivo
-				#java -jar "/home/tiago/Repositorios/Github/Maximum-Diversity-Problem/Projeto/MaximumDiversityProblem/dist/MaximumDiversityProblem.jar" 1 1 1 $arquivo --nr 307 --const gp --p 0.13 --search fis
+				echo "$i-$arquivo"
+				java -jar "/home/usuario/Repositorios/Github/Maximum-Diversity-Problem/Projeto/MaximumDiversityProblem/dist/MaximumDiversityProblem.jar" 1 1 1 $arquivo --nr 350 --const gp --p 0.13 --search fis
 			fi
 		done
 done
