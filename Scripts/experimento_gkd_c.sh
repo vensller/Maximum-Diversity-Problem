e993277d8b2f2b6f0c84b89d87d84f3ba7fedc27
Tiago Funk <tiagoff.tf@gmail.com>
2018-11-19 17:11:36 -0200

    Scripts para o experimento final

---
+++ Scripts/experimento_gkd_c.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+do
+	echo "\n\n\n\n\nInteração $i"
+	for arquivo in /home/usuario/Downloads/mdplib_2010/GKD_c/*
+		do
+			if [ -f "$arquivo" ]
+			then
+				echo $arquivo
+				#java -jar "/home/tiago/Repositorios/Github/Maximum-Diversity-Problem/Projeto/MaximumDiversityProblem/dist/MaximumDiversityProblem.jar" 1 1 1 $arquivo --nr 307 --const gp --p 0.13 --search fis
+			fi
+		done
+done
+
